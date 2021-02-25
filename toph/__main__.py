bfee7f96c67d18f79ba1c4ff15adbb43fa5564a5
Boris Lopez <boris.lopez@walmart.cl>
2021-02-25 13:15:33 -0300

    se agrega manejador de control + C

--- toph/__main__.py
+++ toph/__main__.py
@@ -1,4 +1,11 @@
+from signal import signal, SIGINT
+from sys import exit
 from toph import app
 
+def handler(signal_received, frame):
+    print(' CTRL-C detected. Exiting gracefully')
+    exit(0)
+
 if __name__ == "__main__":
+    signal(SIGINT, handler)
     app.run()
