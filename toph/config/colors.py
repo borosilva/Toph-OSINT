2d7de83402c656213cdee1b6ea00e1f80e0784d5
Boris Lopez <boris.lopez@walmart.cl>
2021-03-04 17:27:57 -0300

    se agrega nueva conf para colores

---
+++ toph/config/colors.py
@@ -0,0 +1,9 @@
+class colors:
+    header = '\033[95m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    alert = '\033[93m'
+    fail = '\033[91m'
+    normal = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
