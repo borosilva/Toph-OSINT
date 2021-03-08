5c8b4902b5150322362f37af6a4d4113780b537a
Boris Lopez <boris.lopez@walmart.cl>
2021-03-08 18:58:53 -0300

    se elimina selenium

--- toph/open_sources/twitter/client/twitter_client.py
+++ toph/open_sources/twitter/client/twitter_client.py
@@ -3,22 +3,13 @@ from toph.config import colors
 from os import getenv
 from bs4 import BeautifulSoup
 from requests import get
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 def getData(username):
     try:
         TWITTER_URL = getenv("TWITTER_URL")
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        PATH = "/Users/vn5139c/Desktop/chromedriver"
-        driver = webdriver.Chrome(PATH, options = options)
 
         url = TWITTER_URL + username
-        driver.get(url)
-        html = driver.page_source
-        print(html)
+        html = get(url).text
         soup = BeautifulSoup(html, "html.parser")
         for text in soup.findAll("h1"):
             text = regex.removeTags(str(text))
