a114b663d51144ef5a783d4d71832e251b5f5f02
Boris Lopez <boris.lopez@walmart.cl>
2021-03-08 18:54:11 -0300

    se reemplaza funcion de panda por nuevas de impresion

--- toph/open_sources/email_rep/email_rep.py
+++ toph/open_sources/email_rep/email_rep.py
@@ -1,35 +1,22 @@
-from toph.common import exceptions
+from toph.common import exceptions, prints
 from toph.open_sources.email_rep.client import email_rep_client
-import numpy as np
-import pandas as pd
 
 def checkEmailRep(email):
     try:
+        TITLE = "EMAIL REP"
+
         dataJson = email_rep_client.getData(email)
         DOMAIN = email.split("@")
-        s = pd.Series(
-                [
-                    str(dataJson["reputation"]), 
-                    str(dataJson["suspicious"]),
-                    str(dataJson["details"]["blacklisted"]),
-                    str(dataJson["details"]["malicious_activity"]),
-                    str(DOMAIN[1]),
-                    str(dataJson["details"]["domain_exists"]),
-                    str(dataJson["details"]["first_seen"]),
-                    str(dataJson["details"]["profiles"])
-                ], 
-                index=[
-                    "Reputation:", 
-                    "Suspicious:",
-                    "Black listed:",
-                    "Malicious Activity:",
-                    "Domain:",
-                    "Domain Exists:",
-                    "First Seen:",
-                    "Profiles:"
-                ]
-            )
 
-        print("\n",s)
+        prints.titlePrint(TITLE)
+        prints.infoPrint("Reputation", str(dataJson["reputation"]))
+        prints.infoPrint("Suspicious", str(dataJson["suspicious"]))
+        prints.infoPrint("Black listed", str(dataJson["details"]["blacklisted"]))
+        prints.infoPrint("Malicious Activity", str(dataJson["details"]["malicious_activity"]))
+        prints.infoPrint("Domain", str(DOMAIN[1]))
+        prints.infoPrint("Domain Exists", str(dataJson["details"]["domain_exists"]))
+        prints.infoPrint("First Seen", str(dataJson["details"]["first_seen"]))
+        prints.infoPrint("Profiles", str(dataJson["details"]["profiles"]))
+        
     except ValueError:
         exceptions.printException(__name__)
