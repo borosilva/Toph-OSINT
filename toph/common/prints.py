50b468ffa48a703e6d0a231014628ab7cdd53401
Boris Lopez <boris.lopez@walmart.cl>
2021-03-08 18:52:49 -0300

    se agregan funciones de impresion

--- toph/common/prints.py
+++ toph/common/prints.py
@@ -1,4 +1,17 @@
 from os import system, name
+from toph.config import colors
 
 def clearPrint():
     system('cls' if name == 'nt' else "printf '\033c'")
+
+def titlePrint(source):
+    print((colors.colors.header + "########################### {} ###########################" + colors.colors.normal).format(source))
+
+def infoPrint(key, value):
+    print((colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + "{}").format(key.upper(), value))
+
+def foundPrint(source, url):
+    print((colors.colors.green + "[FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + "{}").format(source, url))
+
+def notFoundPrint(source, url):
+    print((colors.colors.alert + "[NOT FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + "{}").format(source, url))
