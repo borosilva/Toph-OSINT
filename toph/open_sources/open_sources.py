05b3e29cad989f5fd027e4e625d74cd2dca81cea
Boris Lopez <boris.lopez@walmart.cl>
2021-03-25 12:36:33 -0300

    se encapsula la busqueda por fuentes abiertas

---
+++ toph/open_sources/open_sources.py
@@ -0,0 +1,31 @@
+from toph.open_sources.email_rep import email_rep
+
+from toph.open_sources.instagram import instagram
+from toph.open_sources.facebook import facebook
+from toph.open_sources.twitter import twitter
+from toph.open_sources.youtube import youtube
+from toph.open_sources.pinterest import pinterest
+from toph.open_sources.flickr import flickr
+from toph.open_sources.medium import medium
+from toph.open_sources.github import github
+from toph.open_sources.about_me import about_me
+from toph.open_sources.spotify import spotify
+from toph.open_sources.telegram import telegram
+from toph.open_sources.tripadvisor import tripadvisor
+
+def searchOnAllOpenSourcesByEmail(email):
+    email_rep.checkEmailRep(email)
+
+def searchOnAllOpenSourcesByUsername(username):
+    instagram.checkByUserName(username)
+    facebook.checkByUserName(username)
+    twitter.checkByUserName(username)
+    youtube.checkByUserName(username)
+    pinterest.checkByUserName(username)
+    flickr.checkByUserName(username)
+    medium.checkByUserName(username)
+    github.checkByUserName(username)
+    about_me.checkByUserName(username)
+    spotify.checkByUserName(username)
+    telegram.checkByUserName(username)
+    tripadvisor.checkByUserName(username)
