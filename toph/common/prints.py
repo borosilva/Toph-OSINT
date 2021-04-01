c57c62e4204433cb83eee6780af97744d817705f
Boris Lopez <boris.lopez@walmart.cl>
2021-04-01 13:40:10 -0300

    se agrega fin de linea

--- toph/common/prints.py
+++ toph/common/prints.py
@@ -8,13 +8,13 @@ def titlePrint(source):
     print((colors.colors.header + "########################### {} ###########################" + colors.colors.normal).format(source))
 
 def infoPrint(key, value):
-    print((colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + "{}").format(key.upper(), value))
+    print(("\r" + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + "{}").format(key.upper(), value))
 
 def foundPrint(source, username, url):
-    print((colors.colors.green + "[FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + colors.colors.normal + " {} " + "\n").format(source, username, url))
+    print(("\r" + colors.colors.green + "[FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + colors.colors.normal + " {} " + "\n").format(source, username, url))
 
 def notFoundPrint(source, url):
-    print((colors.colors.alert + "[NOT FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + "\n").format(source, url))
+    print(("\r" + colors.colors.alert + "[NOT FOUND] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + "\n").format(source, url))
 
 def notAvailablePrint(source, url):
-    print((colors.colors.fail + "[NOT AVAILABLE] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + "\n").format(source, url))
+    print(("\r" + colors.colors.fail + "[NOT AVAILABLE] " + colors.colors.blue + colors.colors.bold + " {}: " + colors.colors.normal + " {} " + "\n").format(source, url))
