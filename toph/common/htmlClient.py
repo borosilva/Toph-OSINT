62903747f84310513412a8da35661d337ea3c5e6
Boris Lopez <boris.lopez@walmart.com>
2021-09-14 19:08:44 -0300

    header is added

--- toph/common/htmlClient.py
+++ toph/common/htmlClient.py
@@ -5,10 +5,11 @@ from requests import get
 
 def getHtml(url, username):
     try:
+        HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
         PARAMS = dict(lang='en-US')
         
         urlComplete = url + username
-        html = get(urlComplete, params=PARAMS).text
+        html = get(urlComplete, params=PARAMS, headers=HEADERS).text
         parsedHtml = BeautifulSoup(html, "html.parser")
         return parsedHtml
     except ValueError:
